333dcae87bb21a7ef3edd25d0ee6190d42eacdf1
Donald Mennerich <donaldmennerich@nypl.org>
2012-08-01 11:54:20 -0400

    interface/jquery

--- test.rb
+++ test.rb
@@ -1,20 +1,12 @@
 require './lib/mongo'
 require './lib/dfxml'
 require './lib/dfxml/parser'
-require './lib/file_manager'
+require './lib/dfxml_mongoizer'
 
-dfxml = DfxmlProcessor::DfxmlFile.new('resources/M1126-0007.001')
 mongo = MongoDFXML::DfxmlDB.new
 
 mongo.db.remove()
+dfxml = DfxmlProcessor::DfxmlFile.new('resources/M1126-0022.xml')
 mongo.db.save(dfxml.image_hash)
-result = mongo.db.find(:image_filename => dfxml.image_filename)
-printf(dfxml.image_filename + " [" + dfxml.image_fstype + "]\n")
-
-if(result.has_next?)
-  result.next['files'].each do |file|
-    if file['pronom_format'] != nil
-      puts "\t" << file['filename'] << "\t" << file['pronom_format'].to_s
-    end
-  end
-end
+dfxml = DfxmlProcessor::DfxmlFile.new('resources/M18400-0007.xml')
+mongo.db.save(dfxml.image_hash)
